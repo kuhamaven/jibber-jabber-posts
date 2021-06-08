507495882eaf5c123c8c52d1c5bf83454a224a44
khalil <khalil.stessens@ing.austral.edu.ar>
2021-06-08 11:24:14 -0300

    Implemented posts full logic

--- src/main/java/group7/jibberjabber/posts/posts/PostsRepository.java
+++ src/main/java/group7/jibberjabber/posts/posts/PostsRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 @Repository
 public interface PostsRepository extends PagingAndSortingRepository<PostsModel,Integer> {
-    Optional<PostsModel> findById(int id);
-    Iterable<PostsModel> findAllByAuthor(String author);
-    Iterable<PostsModel> findAll();
+    Optional<PostsModel> findById(String id);
+    List<PostsModel> findAllByAuthor(String author);
+    List<PostsModel> findAllByAuthorId(String authorId);
+    List<PostsModel> findAllByLikedBy(String id);
+    List<PostsModel> findAll();
 
 }
